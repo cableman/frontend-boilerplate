1179efc6010cd0ec83a56cf4bf0026ace0a3a943
Jesper Kristensen <cableman@linuxdev.dk>
2014-02-04 16:33:49 +0100

    Added html template to add meta tag

---
+++ templates/html.tpl.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Implementation to display the basic html structure of a single Drupal page.
+ *
+ */
+?>
+
+<html>
+<head>
+  <meta charset="utf-8">
+  <?php print $head; ?>
+  <meta name="viewport" content="width=device-width">
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
