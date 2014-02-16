eb2e8bce8a76bf9cfd469f7af6ca468d4fff2d0b
Jesper Kristensen <cableman@linuxdev.dk>
2014-02-16 14:07:24 +0100

    Added first version of mobile menu

--- script/script.js
+++ script/script.js
@@ -1,16 +1,24 @@
 (function ($) {
   $(document).ready(function() {
-    enquire.register("screen and (max-width:768px)", function() {
+    enquire.register("screen and (max-width:768px)", function () {
       // Move menu items.
       var sec_menu = $('#block-menu-block-2 .menu');
       var main_menu = $('#block-menu-block-1 .menu');
       var items = $('li', sec_menu);
 
       // Append menu item to main menu.
-      main_menu.append($('li', sec_menu));
+      main_menu.append(items);
 
-      // Remove the sec. menu.
-      $('#block-menu-block-2').remove();
+      // Add mobile menu icon
+      sec_menu.append('<div class="js-mobile--menu"><i class="icon-menu"></i>Menu</div>');
+
+      // Hide main menu.
+      main_menu.hide();
+
+      // Toggle menu.
+      $('.js-mobile--menu').click(function () {
+        main_menu.toggle();
+      });
     });
   });
 })(jQuery);
