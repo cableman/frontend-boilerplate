786da47bcd405e9e2854170f5c278044c1232326
Jesper Kristensen <cableman@linuxdev.dk>
2014-02-20 23:10:12 +0100

    Tried to make script load faster in IE8

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,42 +1,44 @@
 (function ($) {
-  $(document).ready(function () {
 
+Modernizr.load({
+  load: [
+    '/sites/andromeda.dk/themes/nebula/scripts/respond.min.js',
+    '/sites/andromeda.dk/themes/nebula/scripts/respond.matchmedia.addListener.min.js'
+  ],
+  complete: function () {
     Modernizr.load({
       load: [
-        '/sites/andromeda.dk/themes/nebula/scripts/respond.min.js',
-        '/sites/andromeda.dk/themes/nebula/scripts/respond.matchmedia.addListener.min.js'
+        "/sites/andromeda.dk/themes/nebula/scripts/matchMedia.min.js",
+        "/sites/andromeda.dk/themes/nebula/scripts/enquire.min.js"
       ],
       complete: function () {
-        Modernizr.load({
-          load: [
-            "/sites/andromeda.dk/themes/nebula/scripts/matchMedia.js",
-            "/sites/andromeda.dk/themes/nebula/scripts/enquire.min.js"
-          ],
-          complete: function () {
-            enquire.register("only screen and (max-width:767px)", function () {
-              // Move menu items.
-              var sec_menu = $('#block-menu-block-2 .menu');
-              var main_menu = $('#block-menu-block-1 .menu');
-              var items = $('li', sec_menu);
+        $(document).ready(function () {
 
-              // Append menu item to main menu.
-              main_menu.append(items);
+          enquire.register("only screen and (max-width:767px)", function () {
+            // Move menu items.
+            var sec_menu = $('#block-menu-block-2 .menu');
+            var main_menu = $('#block-menu-block-1 .menu');
+            var items = $('li', sec_menu);
 
-              // Add mobile menu icon
-              sec_menu.append('<div class="js-mobile--menu"><i class="icon-menu"></i>Menu</div>');
-              sec_menu.show();
+            // Append menu item to main menu.
+            main_menu.append(items);
 
-              // Toggle menu.
-              $('.js-mobile--menu').click(function () {
-                main_menu.toggle();
-              });
+            // Add mobile menu icon
+            sec_menu.append('<div class="js-mobile--menu"><i class="icon-menu"></i>Menu</div>');
+            sec_menu.show();
 
-              // Fix sec. menu.
-              $('.secondary-content .menu-name-main-menu .menu').ReSmenu();
+            // Toggle menu.
+            $('.js-mobile--menu').click(function () {
+              main_menu.toggle();
             });
-          },
+
+            // Fix sec. menu.
+            $('.secondary-content .menu-name-main-menu .menu').ReSmenu();
+          });
         });
       }
     });
-  });
+  }
+});
+
 })(jQuery);
